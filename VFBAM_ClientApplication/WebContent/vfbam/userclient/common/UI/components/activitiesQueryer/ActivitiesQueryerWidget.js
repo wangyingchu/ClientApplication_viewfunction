89686a0566ce4dedb4ace10eec7fa2b75795ac44
wangychu <wangychu@ADMINIB-DTPQSIC>
2015-03-01 22:37:57 +0800

    query activity by id feature

--- VFBAM_ClientApplication/WebContent/vfbam/userclient/common/UI/components/activitiesQueryer/ActivitiesQueryerWidget.js
+++ VFBAM_ClientApplication/WebContent/vfbam/userclient/common/UI/components/activitiesQueryer/ActivitiesQueryerWidget.js
@@ -43,6 +43,44 @@ require([
             };
             dojo.connect(dialog,"hide",closeDialogCallBack);
         },
+        queryActivityById:function(){
+            var activityIdValue=this.queryActivityIdInput.get("value");
+            if(activityIdValue==""){
+                UI.showToasterMessage({type:"warning",message:"请输入业务活动序号"});
+                return;
+            }else{
+                var resturl=ACTIVITY_SERVICE_ROOT+"activityInstanceDetail/"+APPLICATION_ID+"/"+activityIdValue+"/";
+                var errorCallback= function(data){
+                    UI.showSystemErrorMessage(data);
+                };
+                var that=this;
+                var loadCallback=function(data){
+                    that.doShowActivityInstanceDetail(data,activityIdValue);
+                };
+                Application.WebServiceUtil.getJSONData(resturl,true,null,loadCallback,errorCallback);
+            }
+        },
+        doShowActivityInstanceDetail:function(activityInstanceData,activityIdForQuery){
+            if(!activityInstanceData.activityId){
+                UI.showToasterMessage({type:"error",message:"不存在序号为 <b>"+activityIdForQuery+"</b> 的业务活动"});
+                return;
+            }
+            this.queryActivityIdInput.set("value","");
+            var activityInstanceDetail=new vfbam.userclient.common.UI.components.activitiesQueryer.ActivityInstanceDetailWidget({activityInstanceData:activityInstanceData});
+            var	dialog = new Dialog({
+                style:"width:760px;",
+                title: "<i class='icon-info-sign'></i> 业务活动详情",
+                content: "",
+                buttons:null,
+                closeButtonLabel: "<i class='icon-remove'></i> 关闭"
+            });
+            dojo.place(activityInstanceDetail.containerNode, dialog.containerNode);
+            dialog.show();
+            var closeDialogCallBack=function(){
+                activityInstanceDetail.destroy();
+            };
+            dojo.connect(dialog,"hide",closeDialogCallBack);
+        },
         _endOfCode: function(){}
     });
 });
